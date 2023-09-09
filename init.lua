879113824d98c6c9a88c2db09a384c50713b3989
Khoa Nguyen Do <khoanguyen.do@outlook.com>
2023-09-09 11:43:18 +0700

    initial commit

---
+++ init.lua
@@ -0,0 +1,3 @@
+require('catou.set') -- Settings
+require('catou.remap') -- Remap
+require('catou.lazy') -- Lazy (package manager)
---
+++ lua/catou/lazy.lua
@@ -0,0 +1,31 @@
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", -- latest stable release
+    lazypath,
+  })
+end
+vim.opt.rtp:prepend(lazypath)
+
+require("lazy").setup({
+    {"nvim-treesitter/nvim-treesitter", build = ":TSUpdate"},
+
+    "williamboman/mason.nvim",
+    "williamboman/mason-lspconfig.nvim",
+    "neovim/nvim-lspconfig",
+
+    "hrsh7th/cmp-nvim-lsp",
+    "hrsh7th/cmp-buffer",
+    "hrsh7th/cmp-path",
+    "hrsh7th/cmp-cmdline",
+    "hrsh7th/nvim-cmp",
+
+    "hrsh7th/cmp-vsnip",
+    "hrsh7th/vim-vsnip",
+
+    { "catppuccin/nvim", name = "catppuccin", priority = 1000 }
+})
